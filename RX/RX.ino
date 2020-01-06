0c73fa3044b83e7c16369c913b3638839292403c
Deuter <wojciechbernat95@gmail.com>
2020-01-06 20:35:42 +0100

    Added main RX code file

---
+++ RX/RX.ino
@@ -0,0 +1,131 @@
+#include <Arduino.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <nRF24l01.h>
+#include <printf.h>
+
+//#include "libbuf.h"
+//#include "lib-adc-irq.h"
+
+#define CE 7
+#define CSN 8
+
+#define TX_BUF_SIZE 32     //Size of TX buffer
+#define RX_BUF_SIZE 32     //Size of RX buffer
+#define ADDRESS_BUF_SIZE 5 //Size of buffer with pipeline address
+#define TO_SEND_SIZE 5     //Size of data to send
+const uint8_t addressRX[ADDRESS_BUF_SIZE] = {'P', 'I', 'L', 'O', 'T'};
+const uint8_t addressTX[ADDRESS_BUF_SIZE] = {'S', 'L', 'A', 'V', 'E'};
+
+uint8_t bufferTX[TX_BUF_SIZE];
+uint8_t bufferRX[RX_BUF_SIZE];
+
+uint32_t ackCounter = 0;
+uint32_t *pAckCounter = &ackCounter;
+
+RF24 RX(CE, CSN); //set CE, CSN pins
+
+/* funtions's prototypes */
+void resetShort(uint8_t *buf, size_t size);
+void printBuffer(uint8_t *buf, size_t size);
+void updateCounter(uint32_t *counter);
+
+void setup()
+{
+
+  Serial.begin(115200);
+  Serial.println("Serial port init done.");
+
+  /* RF config */
+  Serial.println("RF init start.\n");
+  RX.begin();
+  RX.setPALevel(RF24_PA_LOW); //set Power Amp output power
+  Serial.println("Set PA power.");
+  RX.setDataRate(RF24_250KBPS); //set Data Rate of speed tramission
+  Serial.println("Set data rate.");
+  RX.setChannel(0x64); //set Channel
+  Serial.println("Set RF channel.");
+
+  RX.setRetries(4, 3); //set Retries of package trasmission
+  Serial.println("Set retries feature.");
+  RX.setCRCLength(RF24_CRC_8);
+  Serial.println("Set CRC.");
+
+  RX.setAutoAck(1);
+  Serial.println("Enable ACK.");
+  RX.enableDynamicPayloads() ;
+  Serial.println("Enable Dynamic Payload size.");
+  RX.enableAckPayload(); //enable transmission ACK signal with Payload
+  Serial.println("Enable ACK payload.");
+  RX.enableDynamicPayloads();
+  Serial.println("Enable dynamic payload size.");
+  Serial.println("nRF24 init done.");
+
+  /* Open pipeline */
+  RX.openWritingPipe(addressTX);    //enable pipeline to transmit on TX address
+  RX.openReadingPipe(1, addressRX); //enable pipeline to receive from RX address
+
+  Serial.println("nRF24 pipeline addresses done.");
+
+  /* Reset MCU TX and RX buffers */
+  resetShort(bufferTX, TX_BUF_SIZE);
+  resetShort(bufferRX, RX_BUF_SIZE);
+  printBuffer(bufferTX, TX_BUF_SIZE);
+  printBuffer(bufferRX, RX_BUF_SIZE);
+  Serial.println("TX and RX buffers reset done.\n");
+  bufferTX[0] = 65;  // 'A'
+  bufferTX[1] = 68;  // 'D';
+  bufferTX[2] = 72;  // 'K';                                 //init ACK - Payload message
+  bufferTX[4] = 0x0A; //EOL
+  RX.writeAckPayload(1, bufferTX, sizeof(bufferTX)); //prepare msg to send
+
+  RX.startListening();
+}
+
+void loop()
+{
+  if (RX.available())
+  {
+    uint8_t lenghtRX = RX.getDynamicPayloadSize();
+    RX.read(bufferRX, lenghtRX);
+    Serial.print("\nReceived data.");
+    printBuffer(bufferRX, RX_BUF_SIZE);
+    bufferTX[3] = ackCounter;
+    RX.writeAckPayload(1, &bufferTX, TO_SEND_SIZE);
+    Serial.print("\nTransmitted data.");
+    printBuffer(bufferTX, TX_BUF_SIZE);
+    ackCounter ++;
+    if (ackCounter == 4294967295) {
+      ackCounter;
+    }
+  }
+}
+
+void updateCounter(uint32_t *counter)
+{
+  *counter++;
+  if (*counter == 4294967295)
+    *counter = 0;
+}
+
+void printBuffer(uint8_t *buf, size_t size)
+{
+  Serial.print("\nBuffer content.");
+  for (size_t i = 0; i < size; i++)
+  {
+    Serial.print(buf[i]);
+    Serial.print("\t");
+    if (i % 2)
+    {
+      Serial.print("\n");
+    }
+  }
+}
+
+void resetShort(uint8_t *buf, size_t size)
+{
+  for (size_t i = 0; i < size; i++)
+  {
+    buf[i] = 0;
+  }
+}
