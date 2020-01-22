df207c86e46685ea4e3ec5e66a3e5a99be70e15b
Deuter <wojciechbernat95@gmail.com>
2020-01-22 22:09:07 +0100

    Added check-functions to check all movements states

--- RX/checkFunTank.c
+++ RX/checkFunTank.c
@@ -4,14 +4,31 @@
 #define Y_LOW 120
 #define Y_HIGH 130
 
+/**
+ *  x value below 120 means left
+ * x value above 130 means right
+ * y value below 120 means forward
+ * y value above 130 means revers 
+*/
+
+/* check basic movments */
 bool checkForward(uint8_t x, uint8_t y);
 bool checkRevers(uint8_t x, uint8_t y);
 bool checkLeft(uint8_t x, uint8_t y);
 bool checkRight(uint8_t x, uint8_t y);
+bool checkStop(uint8_t x, uint8_t y);
 
+/* check advanced movments */
+bool checkForwardLeft(uint8_t x, uint8_t y);
+bool checkForwardRight(uint8_t x, uint8_t y);
+bool checkReversLeft(uint8_t x, uint8_t y);
+bool checkReversRight(uint8_t x, uint8_t y);
+
+/* auxiliary functions */
 bool xCheckStopPosition(uint8_t x);
 bool yCheckStopPosition(uint8_t y);
 
+/* check basic movments */
 bool checkForward(uint8_t x, uint8_t y)
 {
     if ((y < Y_LOW) && (xCheckStopPosition(x)))
@@ -48,6 +65,52 @@ bool checkRight(uint8_t x, uint8_t y)
     return false; //not right
 }
 
+bool checkStop(uint8_t x, uint8_t y)
+{
+    if (yCheckStopPosition(y) && xCheckStopPosition(x))
+    {
+        return true;
+    }
+    return false;
+}
+
+/* check advanced movments */
+bool checkForwardLeft(uint8_t x, uint8_t y)
+{
+    if ((y < Y_LOW) && (x < X_LOW))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool checkForwardRight(uint8_t x, uint8_t y)
+{
+    if ((y < Y_LOW) && (x > X_HIGH))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool checkReversLeft(uint8_t x, uint8_t y)
+{
+    if ((y > Y_HIGH) && (x < X_LOW))
+    {
+        return true;
+    }
+    return false;
+}
+bool checkReversRight(uint8_t x, uint8_t y)
+{
+    if ((y > Y_HIGH) && (x > X_HIGH))
+    {
+        return true;
+    }
+    return false;
+}
+
+/* auxiliary functions */
 /**
  * Check that x is in stop position 
  * */
