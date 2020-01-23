9d1707ce4bc4f29f22edc6750f66552a83339d04
Deuter <wojciechbernat95@gmail.com>
2020-01-23 21:12:52 +0100

    improved constants definitions

--- RX/rescale.h
+++ RX/rescale.h
@@ -4,6 +4,10 @@
  * 
  * */
 
+#define ADC_HIGH 255
+#define ADC_MID_HIGH 128
+#define ADC_MID_LOW 127
+
 uint8_t hightToLow(uint8_t a); // a > reserved_interval i.e. ADC = 129 convert to PWM = 2
 uint8_t invert(uint8_t a);     // a < reserved_interval i.e. ADC = 0 convert to PWM = 255
 uint8_t invertHigh(uint8_t a);
